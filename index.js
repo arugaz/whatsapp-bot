d73b3c14a4bef944475b562a274b69beb2b5d679
ArugaZ <arugaastri@gmail.com>
2020-10-30 12:25:57 +0000

    update islam commands

--- index.js
+++ index.js
@@ -403,7 +403,7 @@ const start = (aruga = new Client()) => {
         case 'tts':
             if (args.length == 0) return aruga.reply(from, `Mengubah teks menjadi sound (google voice)\nketik: ${prefix}tts <kode_bahasa> <teks>\ncontoh : ${prefix}tts id halo\nuntuk kode bahasa cek disini : https://anotepad.com/note/read/5xqahdy8`)
             const ttsGB = require('node-gtts')(args[0])
-            const dataText = body.slice(9)
+            const dataText = body.slice(8)
                 if (dataText === '') return aruga.reply(from, 'apa teksnya syg..', id)
                 try {
                     ttsGB.save('./media/tts.mp3', dataText, function () {
