29f874555d44b97ebc8d111d52eec0e8f1c88972
ArugaZ <arugaastri@gmail.com>
2020-11-09 08:21:51 +0000

    UPDATE OPTIONS.JS!

--- utils/options.js
+++ utils/options.js
@@ -5,38 +5,13 @@
  */
 
 module.exports = options = (headless, start) => {
-    // const chromePath = {
-    //     win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Windows 32 bit
-    //     win64: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', // Windows 64 bit
-    //     linuxChrome: '/usr/bin/google-chrome-stable', // Linux - Chrome
-    //     linuxChromium: '/usr/bin/chromium-browser', // Linux - Chromium
-    //     darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' // MacOS
-    // }
-
-    // if (fs.existsSync(chromePath.win32)) {
-    //     execPath = chromePath.win32
-    // } else if (fs.existsSync(chromePath.win64)) {
-    //     execPath = chromePath.win64
-    // } else if (fs.existsSync(chromePath.linuxChrome)) {
-    //     execPath = chromePath.linuxChrome
-    // } else if (fs.existsSync(chromePath.linuxChromium)) {
-    //     execPath = chromePath.linuxChromium
-    // } else if (process.platform === 'darwin') {
-    //     execPath = chromePath.darwin
-    // } else {
-    //     console.error(new Error('Google Chrome Is Not Installed'))
-    //     process.exit(1)
-    // }
-
     const options = {
+        sessionId: 'ARUGA',
         headless: headless,
-        qrRefreshS: 20,
         qrTimeout: 0,
         authTimeout: 0,
-        autoRefresh: true,
         restartOnCrash: start,
         cacheEnabled: false,
-        // executablePath: execPath,
         useChrome: true,
         killProcessOnBrowserClose: true,
         throwErrorOnTosBlock: false,
@@ -50,6 +25,5 @@ module.exports = options = (headless, start) => {
             '--disk-cache-size=0'
         ]
     }
-
     return options
 }
