26f5376d28de129f68ebd6a35aa1968b07b363da
thoriqazzikra <75126993+thoriqazzikra@users.noreply.github.com>
2020-12-06 10:28:33 +0700

    Create Handlemsg.js
    fitur get link grup
    btw bang itu nama grupnya knpa error yak wkwk jadi tulisan *undefined*

---
+++ Handlemsg.js
@@ -0,0 +1,10 @@
+      case 'linkgroup':
+            case 'linkgc' :
+            if (!isBotGroupAdmins) return aruga.reply(from, 'Perintah ini hanya bisa di gunakan ketika bot menjadi admin', id)
+            if (isGroupMsg) {
+                const inviteLink = await aruga.getGroupInviteLink(groupId);
+                aruga.sendLinkWithAutoPreview(from, inviteLink, `\nLink group *${groupname}*`)
+            } else {
+            	aruga.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
+            }
+            break
