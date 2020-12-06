e1d4c6e9a0dd04ac0d898444f8943b9450c4e658
thoriqazzikra <75126993+thoriqazzikra@users.noreply.github.com>
2020-12-06 13:13:43 +0700

    Update Handlemsg.js

--- Handlemsg.js
+++ Handlemsg.js
@@ -3,7 +3,7 @@
             if (!isBotGroupAdmins) return aruga.reply(from, 'Perintah ini hanya bisa di gunakan ketika bot menjadi admin', id)
             if (isGroupMsg) {
                 const inviteLink = await aruga.getGroupInviteLink(groupId);
-                aruga.sendLinkWithAutoPreview(from, inviteLink, `\nLink group *${groupname}*`)
+                aruga.sendLinkWithAutoPreview(from, inviteLink, `\nLink group *${name}*`)
             } else {
             	aruga.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
             }
