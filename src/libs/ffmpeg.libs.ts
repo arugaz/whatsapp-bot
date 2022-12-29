c59edc00cefb31bac499af2d39ae4feea5fe85f6
ArugaZ <arugaastri@gmail.com>
2022-12-29 13:30:16 +0700

    use stream for ffmpeg input & output

--- src/libs/ffmpeg.libs.ts
+++ src/libs/ffmpeg.libs.ts
@@ -1,23 +1,27 @@
 import { spawn } from "child_process"
-import { removeTEMP, saveTEMP } from "../utils/filesystem.utils"
 
+/**
+ * FFMPEG Spawner functions
+ * @param bufferData Input BUffer
+ * @param options FFMPEG Options
+ * @returns Result Buffer
+ */
 const ffmpeg = (bufferData: Buffer, options: string[]) =>
   new Promise<Buffer>((resolve, reject) => {
-    saveTEMP(bufferData, "ffmpeg").then(inFile => {
-      options = ["-y", "-hide_banner", "-loglevel", "error", "-i", inFile, ...options, "pipe:1"]
-      const spawner = spawn("ffmpeg", options, { windowsHide: true })
-      const result: Buffer[] = []
-
-      spawner.stdout.on("data", (data: Buffer) => result.push(data))
-      spawner.on("error", reject)
-      spawner.on("close", code => {
-        process.nextTick(() => removeTEMP(inFile))
-        if (code !== 0) {
-          return reject(new Error(`Could not complete the process with exit code ${code}`))
-        }
-        return resolve(Buffer.concat(result))
-      })
+    const result = new Array<Uint8Array>()
+    options = ["-hide_banner", "-loglevel", "error", "-i", "pipe:0", ...options, "-y", "pipe:1"]
+    const spawner = spawn("ffmpeg", options, { windowsHide: true })
+    spawner.stdout.on("data", (data: Uint8Array) => result.push(data))
+    spawner.on("error", reject)
+    spawner.on("close", (code, signal) => {
+      process.nextTick(() => spawner.stdin.destroy())
+      if (code !== 0) {
+        return reject(new Error(`Could not complete the process with exit code ${code} & signal ${signal}`))
+      }
+      return resolve(Buffer.concat(result))
     })
+    spawner.stdin.write(bufferData)
+    spawner.stdin.end(() => bufferData.fill(0))
   })
 
 export default ffmpeg
